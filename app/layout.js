6b3f1605e0520efdfcdf09f6924a4b1cd635c995
Mohit Yadav <mohityadavhh@gmail.com>
2025-09-03 19:53:48 +0530

    add feature  current user who visit website info

--- app/layout.js
+++ app/layout.js
@@ -1,7 +1,8 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import Navbar from "@/app/navbar/page"
+import Navbar from "@/app/navbar/page";
 import Footer from "@/app/footer/page";
+import Script from "next/script"; // ✅ Import Script
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -14,13 +15,35 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata = {
-  title: "Mohit Yadav | Full Stack  Web Developer",
-  description: "Portfolio of Mohit Yadav a full stack web developer skilled in React, Next.js, Node.js, and MongoDB. Explore my projects and experience",
+  title: "Mohit Yadav | Full Stack Web Developer",
+  description:
+    "Portfolio of Mohit Yadav a full stack web developer skilled in React, Next.js, Node.js, and MongoDB. Explore my projects and experience",
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        {/* ✅ Google Analytics */}
+        <Script
+          strategy="afterInteractive"
+          src="https://www.googletagmanager.com/gtag/js?id=G-1CM2L028FM"
+        />
+        <Script
+          id="ga-init"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-1CM2L028FM', {
+                page_path: window.location.pathname,
+              });
+            `,
+          }}
+        />
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
